2a23ecee8751245b1882c7c15c754a62f73af9b9
shuibcodes <abdullahshuib124@gmail.com>
2021-01-10 19:02:02 +0000

    timer component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Timer from './Components/Timerr'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pomodoro Timer</h1>
+        <h3>Get Some Work Done</h3>
+        <Timer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Timerr.js
@@ -0,0 +1,56 @@
+import React, {useState,useEffect} from 'react';
+import '../index.css'
+
+const Timer = () => {
+  const initialMinute = 25;
+  const initialSeconds = 0;
+  const [ minutes, setMinutes ] = useState(initialMinute);
+  const [seconds, setSeconds ] =  useState(initialSeconds);
+  const [isActive, setIsActive] = useState(false)
+  useEffect(()=>{
+  let myInterval = setInterval(() => {
+          if (seconds > 1 && isActive) {
+              setSeconds(seconds - 1);
+          }
+          if (seconds === 0) {
+              if (minutes === 0) {
+                  clearInterval(myInterval)
+              } else {
+                  setMinutes(minutes - 1);
+                  setSeconds(59);
+                  
+              }
+          } 
+      }, 1000)
+      return ()=> {
+          clearInterval(myInterval);
+        };
+  },[seconds,minutes, isActive]);
+  
+  function toggle(){
+    setIsActive(!isActive);
+  }
+
+  function reset(){
+      setSeconds(0);
+      setMinutes(25)
+      setIsActive(false)
+  }
+  return (
+    <div>
+    { minutes === 0 && seconds === 0
+        ? <h1>{minutes}: {seconds}{seconds}</h1> 
+        : <h1> {minutes}:{seconds < 10 ?  `0${seconds}` : seconds}</h1> 
+    }
+    <button onClick={toggle} className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
+          {isActive ? 'Pause' : 'Start'}
+    </button>
+    <button className='button' onClick={reset} >Reset</button>
+    </div>
+    
+
+  )
+
+};
+
+export default Timer;
